eec266af0f9a75bc05d34c99b781c0aaaebdac5d
Surendar <vst.in2k@gmail.com>
2021-10-27 22:17:41 +0530

    backend commit-1

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0bd50510985af1cf3ac8e36dd05c7cbd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8d765695-e1bd-4487-a270-f6fc1ab5e492'
+};
